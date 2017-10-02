52be4a4470e0177eb08b9c116ac49efd6d68190f
ishmaelc <chris.a.ishmael@gmail.com>
2017-10-02 18:14:08 -0400

    not much change

--- app.r
+++ app.r
@@ -1,6 +1,35 @@
 
-
+# install.packages('rgdal')
 # install.packages('ggmap')
+# install.packages('leaflet')
+
+library(rgdal)
+
+states <- readOGR("shp/cb_2013_us_state_20m.shp",
+                  layer = "cb_2013_us_state_20m", GDAL1_integer64_policy = TRUE)
+
+school <- readOGR(dsn = '/Users/Chris/Downloads/EDGE_SCHOOLDISTRICT_TL16_SY1516',layer = 'schooldistrict_sy1516_tl16')
+
+library(leaflet)
+
+state.school <- subset(school, school$STATEFP %in% c(
+  '39'
+))
+leaflet(state.school) %>%
+  # addTiles() %>%
+  addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+              opacity = 1, fillOpacity = 0.1,
+              # fillColor = ~colorQuantile("YlOrRd", ALAND)(ALAND),
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE),label = ~NAME)
+
+f <- state.school$NAME
+f %>% sort()
+
+state.school$
+
+class(state.school)
+str(state.school)
 
 library(dplyr)
 library(ggmap)
@@ -24,11 +53,12 @@ grad.geo <- grad.geo %>% mutate(grade4yr = Letter.Grade.of.4.year.Graduation.Rat
 
 # Pass the palette function a data vector to get the corresponding colors
 pal <- colorFactor(c("green", "yellow", "yellow",'yellow','red', 'black'), c('A','B','C','D','F','NR'))
+pal <- colorFactor(topo.colors(7), c('A','B','C','D','F','NR'))
 
 leaflet(grad.geo) %>% 
     addTiles(
       urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
       attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
     ) %>%
-  addCircleMarkers(popup = ~District.Name,radius = 2,color = ~pal(grade4yr),opacity = .9) %>%
+  addCircleMarkers(popup = ~District.Name,lng = ~lon, lat = ~lat, radius = 2,color = ~pal(grade4yr),opacity = .9) %>%
   addLegend(pal = pal,values= grad.geo$grade4yr)
